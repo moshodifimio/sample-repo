a58d19f979d1b483e9a5adc1da028e430b404caa
Marcus Oshodi <147557402+moshodifimio@users.noreply.github.com>
2024-11-12 14:37:53 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
+import os 
+
 print("Marcus. This is your sample repo. You just made a change")
+print("ENVIRONMENT VARIABLE MESSAGE:", os.getenv('MESSAGE'))
+print("ENVIRONMENT VARIABLE NUMBER:", os.getenv('NUMBER'))
